0f8c54110d5ce381aca7164022dd5ec4e85aa9d8
Damien DURO <damien@weweb.io>
2021-01-21 14:15:47 +0100

    version 1

--- src/wwObject.vue
+++ src/wwObject.vue
@@ -1,24 +1,21 @@
 <template>
     <select
+        v-if="content.globalSettings"
         class="ww-form-dropdown"
-        :class="{ 'ww-editing': isEditing }"
-        :name="content.config.name"
-        :required="content.config.required"
-        :multiple="content.config.multiple"
+        :class="{ editing: isEditing }"
+        :name="content.globalSettings.name"
+        :required="content.globalSettings.required"
+        :multiple="content.globalSettings.multiple"
         :style="style"
     >
-        <option value selected disabled>{{ wwLang.getText(content.config.placeholder) }}</option>
-        <option class="option" v-for="option in content.config.options" :key="option.value" :value="option.value">
+        <option value selected disabled>{{ wwLang.getText(content.globalSettings.placeholder) }}</option>
+        <option v-for="option in content.globalSettings.options" :key="option.value" :value="option.value">
             {{ wwLang.getText(option.name) }}
         </option>
     </select>
 </template>
 
 <script>
-/* wwEditor:start */
-import openPopup from './popups';
-/* wwEditor:end */
-
 export default {
     name: '__COMPONENT_NAME__',
     props: {
@@ -28,30 +25,98 @@ export default {
         /* wwEditor:end */
     },
     wwDefaultContent: {
-        config: {
-            name: 'name',
+        globalStyle: {
+            fontSize: '15px',
+            color: 'black',
+        },
+        globalSettings: {
+            name: '',
             required: true,
             multiple: false,
             placeholder: {
-                en: 'Name',
-                fr: 'Nom',
+                en: 'Select',
             },
             options: [
                 {
                     value: 'option',
-                    name: { en: 'option en', fr: 'option fr' },
+                    name: { en: 'option - 1', fr: 'option - 1' },
                 },
                 {
                     value: 'option2',
-                    name: { en: 'option2 en', fr: 'option2 fr' },
+                    name: { en: 'option - 2', fr: 'option - 2' },
                 },
             ],
         },
-        style: {
-            fontSize: 1,
-            color: 'black',
-        },
     },
+    /* wwEditor:start */
+    wwEditorConfiguration() {
+        return {
+            settingsOptions: {
+                name: {
+                    path: 'globalSettings.name',
+                    label: { en: 'Name', fr: 'fr' },
+                    type: 'Text',
+                    options: {
+                        placeholder: 'Name',
+                    },
+                },
+                required: {
+                    path: 'globalSettings.required',
+                    label: { en: 'Required', fr: 'Requis' },
+                    type: 'OnOff',
+                },
+                multiple: {
+                    path: 'globalSettings.multiple',
+                    label: { en: 'Multiple', fr: 'Multiple' },
+                    type: 'OnOff',
+                },
+                ...(function () {
+                    const placeholders = {};
+                    for (const lang of wwLib.$store.getters['websiteData/getPage'].langs) {
+                        placeholders[`placeholder_${lang}`] = {
+                            path: `globalSettings.placeholder.${lang}`,
+                            label: { en: `placeholder (${lang})`, fr: 'fr' },
+                            type: 'Text',
+                            options: {
+                                placeholder: 'firstname',
+                            },
+                        };
+                    }
+                    return placeholders;
+                })(),
+                options: {
+                    path: 'globalSettings.options',
+                    label: { en: 'Options', fr: 'fr' },
+                    type: 'List',
+                    options: {
+                        options: [
+                            {
+                                path: 'value',
+                                type: 'Text',
+                                options: {
+                                    placeholder: 'Value',
+                                },
+                            },
+                            ...(function () {
+                                const options = [];
+                                for (const lang of wwLib.$store.getters['websiteData/getPage'].langs) {
+                                    options.push({
+                                        path: `name.${lang}`,
+                                        type: 'Text',
+                                        options: {
+                                            placeholder: `value ${lang}`,
+                                        },
+                                    });
+                                }
+                                return options;
+                            })(),
+                        ],
+                    },
+                },
+            },
+        };
+    },
+    /* wwEditor:end */
     data() {
         return {
             wwLang: wwLib.wwLang,
@@ -59,23 +124,20 @@ export default {
     },
     computed: {
         isEditing() {
-            return this.wwEditorState.editMode === wwLib.wwSectionHelper.EDIT_MODES.CONTENT;
+            /* wwEditor:start */
+            return this.wwEditorState.editMode === wwLib.wwEditorHelper.EDIT_MODES.EDITION;
+            /* wwEditor:end */
+            // eslint-disable-next-line no-unreachable
+            return false;
         },
         style() {
+            if (!this.content || !this.content.globalStyle) return {};
             return {
-                color: this.content.style.color || 'black',
-                fontSize: `${this.content.style.fontSize || 1}rem`,
+                color: this.content.globalStyle.color || 'black',
+                fontSize: `${this.content.globalStyle.fontSize || '15px'}`,
             };
         },
     },
-    /* wwEditor:start */
-    methods: {
-        async setOptions() {
-            const result = await openPopup(this.content);
-            if (result) this.$emit('update', result);
-        },
-    },
-    /* wwEditor:end */
 };
 </script>
 
@@ -89,7 +151,7 @@ export default {
     text-overflow: ellipsis;
     cursor: pointer;
     /* wwEditor:start */
-    &.ww-editing {
+    &.editing {
         pointer-events: none;
     }
     /* wwEditor:end */
