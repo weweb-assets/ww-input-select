585a3b3a58baa9f2bef17f2ee2a0f0d2a21ac210
Damien DURO <damien@weweb.io>
2021-09-22 14:47:54 +0200

    Migrate config

--- src/wwElement.vue
+++ src/wwElement.vue
@@ -1,16 +1,15 @@
 <template>
     <select
-        v-if="content.globalSettings"
         class="ww-form-dropdown"
         :class="{ editing: isEditing }"
-        :name="content.globalSettings.name"
-        :required="content.globalSettings.required"
-        :multiple="content.globalSettings.multiple"
+        :name="content.name"
+        :required="content.required"
+        :multiple="content.multiple"
         :style="style"
     >
-        <option value selected disabled>{{ wwLang.getText(content.globalSettings.placeholder) }}</option>
-        <option v-for="option in content.globalSettings.options" :key="option.value" :value="option.value">
-            {{ wwLang.getText(option.name) }}
+        <option value selected disabled>{{ content.placeholder }}</option>
+        <option v-for="option in content.options" :key="option.value" :value="option.value">
+            {{ option.name }}
         </option>
     </select>
 </template>
@@ -23,99 +22,6 @@ export default {
         wwEditorState: { type: Object, required: true },
         /* wwEditor:end */
     },
-    wwDefaultContent: {
-        globalStyle: {
-            fontSize: '15px',
-            color: 'black',
-        },
-        globalSettings: {
-            name: '',
-            required: true,
-            multiple: false,
-            placeholder: {
-                en: 'Select',
-            },
-            options: [
-                {
-                    value: 'option',
-                    name: { en: 'option - 1', fr: 'option - 1' },
-                },
-                {
-                    value: 'option2',
-                    name: { en: 'option - 2', fr: 'option - 2' },
-                },
-            ],
-        },
-    },
-    /* wwEditor:start */
-    wwEditorConfiguration() {
-        return {
-            settingsOptions: {
-                name: {
-                    path: 'globalSettings.name',
-                    label: { en: 'Name', fr: 'fr' },
-                    type: 'Text',
-                    options: {
-                        placeholder: 'Name',
-                    },
-                },
-                required: {
-                    path: 'globalSettings.required',
-                    label: { en: 'Required', fr: 'Requis' },
-                    type: 'OnOff',
-                },
-                multiple: {
-                    path: 'globalSettings.multiple',
-                    label: { en: 'Multiple', fr: 'Multiple' },
-                    type: 'OnOff',
-                },
-                ...(function () {
-                    const placeholders = {};
-                    for (const lang of wwLib.wwWebsiteData.getCurrentPage().langs) {
-                        placeholders[`placeholder_${lang}`] = {
-                            path: `globalSettings.placeholder.${lang}`,
-                            label: { en: `placeholder (${lang})`, fr: 'fr' },
-                            type: 'Text',
-                            options: {
-                                placeholder: 'firstname',
-                            },
-                        };
-                    }
-                    return placeholders;
-                })(),
-                options: {
-                    path: 'globalSettings.options',
-                    label: { en: 'Options', fr: 'fr' },
-                    type: 'List',
-                    options: {
-                        options: [
-                            {
-                                path: 'value',
-                                type: 'Text',
-                                options: {
-                                    placeholder: 'Value',
-                                },
-                            },
-                            ...(function () {
-                                const options = [];
-                                for (const lang of wwLib.wwWebsiteData.getCurrentPage().langs) {
-                                    options.push({
-                                        path: `name.${lang}`,
-                                        type: 'Text',
-                                        options: {
-                                            placeholder: `value ${lang}`,
-                                        },
-                                    });
-                                }
-                                return options;
-                            })(),
-                        ],
-                    },
-                },
-            },
-        };
-    },
-    /* wwEditor:end */
     data() {
         return {
             wwLang: wwLib.wwLang,
@@ -130,10 +36,9 @@ export default {
             return false;
         },
         style() {
-            if (!this.content || !this.content.globalStyle) return {};
             return {
-                color: this.content.globalStyle.color || 'black',
-                fontSize: `${this.content.globalStyle.fontSize || '15px'}`,
+                color: this.content.color,
+                fontSize: `${this.content.fontSize}`,
             };
         },
     },
---
+++ ww-config.js
@@ -0,0 +1,66 @@
+export default {
+    editor: {
+        label: { fr: 'Sélecteur de Formulaire', en: 'Form Dropdown' },
+    },
+    properties: {
+        color: {
+            label: { en: 'Color', fr: 'Couleur' },
+            type: 'Color',
+            defaultValue: 'black',
+        },
+        fontSize: {
+            label: { en: 'Font size', fr: 'Taille du texte' },
+            type: 'Length',
+            options: {
+                unitChoices: [
+                    { value: 'px', label: 'px', min: 10, max: 50 },
+                    { value: 'em', label: 'em', min: 1, max: 50 },
+                    { value: 'rem', label: 'rem', min: 1, max: 50 },
+                ],
+            },
+            defaultValue: '15px',
+        },
+        name: {
+            label: { en: 'Name', fr: 'fr' },
+            type: 'Text',
+            section: 'settings',
+            options: { placeholder: 'Name' },
+            defaultValue: '',
+        },
+        required: {
+            label: { en: 'Required', fr: 'Requis' },
+            type: 'OnOff',
+            section: 'settings',
+            defaultValue: true,
+        },
+        multiple: {
+            label: { en: 'Multiple', fr: 'Multiple' },
+            type: 'OnOff',
+            section: 'settings',
+            defaultValue: false,
+        },
+        placeholders: {
+            label: { en: 'Placeholder', fr: 'Placeholder' },
+            type: 'Text',
+            section: 'settings',
+            options: { placeholder: 'firstname' },
+            multilang: true,
+            defaultValue: { en: 'Select' },
+        },
+        options: {
+            label: { en: 'Options', fr: 'fr' },
+            type: 'List',
+            section: 'settings',
+            options: {
+                options: [
+                    { path: 'value', type: 'Text', options: { placeholder: 'Value' } },
+                    { path: 'name', type: 'Text', options: { placeholder: 'Value' }, multilang: true },
+                ],
+            },
+            defaultValue: [
+                { value: 'option', name: { en: 'option - 1', fr: 'option - 1' } },
+                { value: 'option2', name: { en: 'option - 2', fr: 'option - 2' } },
+            ],
+        },
+    },
+};
