0656138a963ebe4a1be7f6044d8a919e25043da1
Dams-d <damien.duro8@gmail.com>
2023-09-21 15:01:32 +0200

    Prevent click

--- src/wwElement.vue
+++ src/wwElement.vue
@@ -60,7 +60,7 @@
 
         <!-- Clear icon shown when the input has at least one selected options -->
         <template #clear>
-            <wwElement v-bind="content.clearIconElement" @click.stop="clear" />
+            <wwElement v-bind="content.clearIconElement" @mousedown.stop="clear" />
         </template>
     </Multiselect>
     <wwText v-else :text="valueLabel"></wwText>
