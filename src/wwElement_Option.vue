32cf593dc47a144088f4849e4ee952b079a269fa
Aurélie Violette <orwel.violette@gmail.com>
2025-05-23 09:24:30 +0200

    Fix select in production

--- src/wwElement_Option.vue
+++ src/wwElement_Option.vue
@@ -20,8 +20,8 @@ import { ref, unref, toValue, inject, computed, watch, onBeforeUnmount, watchEff
 import useAccessibility from './useAccessibility_Option';
 /* wwEditor:start */
 import useEditorHint from './editor/useEditorHint';
-import { areValuesEqual } from './utils';
 /* wwEditor:end */
+import { areValuesEqual } from './utils';
 
 const SELECT_OPTION_ICON =
     '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-check"><path d="M20 6 9 17l-5-5"/></svg>';
